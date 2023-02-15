dc6cf97e6df8360ccb568e9b148fca1112631253
Vector <vector@algorytmika.com>
2023-02-15 21:34:29 +0100

    Git and python tutorials initialized

---
+++ Development/git.py
@@ -0,0 +1,8 @@
+# change user name  email locally 
+git config --local user.name "vector@test.com"
+
+# change user user name locally 
+git config --local user.name "Vector"
+
+# get user name
+git config user.name
---
+++ Development/python_tutorial.py
@@ -0,0 +1,11 @@
+#create virtual enviroment
+py -m venv venv
+
+# activate virtual enviroment
+ .\venv\Scripts\activate
+
+#install
+pip install ipython
+
+# run file in iptyhon
+run test_script.py
