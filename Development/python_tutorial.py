f5bf2551c1a3425d465bd49dbcaa8fbec8613018
Vector <vector@algorytmika.com>
2023-05-14 11:27:36 +0200

    Before splitting

--- Development/python_tutorial.py
+++ Development/python_tutorial.py
@@ -190,6 +190,26 @@ python -m pytest
 # run pytest without warning concerning the report
 python -m pytest --cov
 
+
+
+
+##################################################################
+							COVERAGE
+##################################################################
+
+# install coverage
+pip install coverage
+
+#run our tests using the coverage module instead of directly using the Python interpreter
+python -m coverage run test.py
+
+#generate a code coverage report using the command
+python -m coverage report
+
+# run the test_mock.py unittest file under coverage analysis, collecting data only for mock_tutorial.py.
+python -m coverage run --include=mock_tutorial.py -m unittest test_mock.py
+
+
 ##################################################################
 							GENERAL
 ##################################################################
